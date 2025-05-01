4c6c13dd3290f50e04241afc05e14e2fac226fb7
Oleksandr <oleksandr.albul@outlook.com>
2025-05-01 11:14:11 +0300

    Add solution and entry point

---
+++ Acta.Tool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Acta.Tool;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var version = Assembly
+			.GetEntryAssembly()?
+			.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+			.InformationalVersion;
+
+		Console.WriteLine($"Acta v{version}");
+	}
+}
